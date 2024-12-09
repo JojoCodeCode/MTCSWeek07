72465e62d1a4ae8c9c1dc1dd040d1905caae3f66
Your Name <your.email@example.com>
2024-12-08 19:45:57 -0700

    first commit

---
+++ main/java/projects/service/ProjectDao.java
@@ -0,0 +1,12 @@
+package projects.service;
+
+import projects.entity.Project;
+
+public class ProjectDao {
+
+	public Project insertProject(Project project) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
