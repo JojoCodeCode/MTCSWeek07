828bd2f47a785c8ba9d04d0bbb80e4115f23086a
Your Name <your.email@example.com>
2024-12-12 21:29:45 -0700

    first commit

--- main/java/projects/service/ProjectDao.java
+++ main/java/projects/service/ProjectDao.java
@@ -1,5 +1,8 @@
 package projects.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import projects.entity.Project;
 
 public class ProjectDao {
@@ -9,4 +12,17 @@ public class ProjectDao {
 		return null;
 	}
 
-}
+	public List<Project> fetchAllProjects() {
+		List<Project> projects = new ArrayList<>(); 
+				
+		return projects;
+	}
+
+	public Object fetchProjectById(Integer projectId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	}
+
+
